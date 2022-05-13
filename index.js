e0b65bc7f3fc897582609255e9ce4ad79cf30639
Mason Wagoner <mgwagoner@gmail.com>
2022-05-13 11:48:05 -0400

    Added Public Folder

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// Modules and Globals
 require('dotenv').config()
 // require express
 const express = require('express');
 // initialize app variable
 const app = express();
 
+// Express Settings
+app.set('views', _dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 // Home Page route
@@ -19,5 +24,5 @@ app.get('*', (req, res) => {
   res.render('error404')
 });
 
-
+// Listen for Connections
 app.listen(process.env.PORT);
